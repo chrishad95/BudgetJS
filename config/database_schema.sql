05faaafc12ee7ed0b4925049638affd93b41a8f9
Chris Hadley <chrishadley@gmail.com>
2014-03-17 07:53:57 -0600

    added database schema code

---
+++ config/database_schema.sql
@@ -0,0 +1,125 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `owner` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget_transfers`
+--
+
+DROP TABLE IF EXISTS `budget_transfers`;
+CREATE TABLE IF NOT EXISTS `budget_transfers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `t_date` date NOT NULL,
+  `source_category_id` int(11) NOT NULL,
+  `destination_category_id` int(11) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `updated_by` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `owner` varchar(50) NOT NULL,
+  `updated_by` varchar(50) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imported_transactions`
+--
+
+DROP TABLE IF EXISTS `imported_transactions`;
+CREATE TABLE IF NOT EXISTS `imported_transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `t_date` date NOT NULL,
+  `payee` varchar(100) NOT NULL,
+  `memo` varchar(256) DEFAULT NULL,
+  `amount` float NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_by` varchar(50) NOT NULL,
+  `check_number` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=141 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `t_date` date NOT NULL,
+  `payee` varchar(100) NOT NULL,
+  `memo` varchar(256) DEFAULT NULL,
+  `amount` float NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_by` varchar(50) NOT NULL,
+  `check_number` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `activated` int(11) NOT NULL,
+  `banned` int(11) NOT NULL,
+  `banned_reason` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `last_login` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
